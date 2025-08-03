13317b9e08e99b5f94ae520f238852d837cd5700
mobinabageri <m.bageri2008@gmail.com>
2025-08-03 12:29:25 +0330

    create main component and add slider

---
+++ frontend/src/app/main/main.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SliderComponent } from "./slider/slider.component";
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [SliderComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+
+}
---
+++ frontend/src/app/main/slider/slider.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slider',
+  standalone: true,
+  imports: [],
+  templateUrl: './slider.component.html',
+  styleUrl: './slider.component.css'
+})
+export class SliderComponent {
+SLIDEIMAGE ='smiling-construction-engineer-posing-with-arms-crossed-isolated-grey-background-1.png'
+}
